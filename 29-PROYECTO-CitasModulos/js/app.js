9a3860a69626645b7ddd3e8b9b06f4ac2ad9879e
pulidoibarra15@gmail.com <pulidoibarra15@gmail.com>
2023-11-29 19:59:55 -0500

    llenar base de datos en Js

--- 29-PROYECTO-CitasModulos/js/app.js
+++
@@ -1,3 +0,0 @@
-import App from './clases/App.js'
-
-const app = new App();
--- Js/db.js
+++ Js/db.js
@@ -1,34 +1,90 @@
 window.FirstProjects = [
     {
         id: 1,
-        titulo: 'Proyecto 1',
-        imagenURL: './public/images/veterinaria1.jpg',
-        alt: 'Imagen proyecto 1',
+        titulo: 'Carrito',
+        imagenURL: './public/images/proyecto-1.PNG',
+        alt: 'Imagen proyecto 2',
         botonTexto: 'Ver proyecto',
-        botonURL: './15-PROYECTO-Carrito/index.html'
+        botonURL: '/PROYECTO-Carrito-LS/index.html'
     },
     {
         id: 2,
-        titulo: 'Proyecto 2',
-        imagenURL: './public/images/veterinaria1.jpg',
-        alt: 'Imagen proyecto 1',
+        titulo: 'Enviar Mensaje',
+        imagenURL: './public/images/proyecto-2.PNG',
+        alt: 'Imagen proyecto 2',
         botonTexto: 'Ver proyecto',
-        botonURL: './index.html'
+        botonURL: '/PROYECTO-EnviarEmail/index.html'
     },
     {
         id: 3,
-        titulo: 'Proyecto 3',
-        imagenURL: './public/images/veterinaria1.jpg',
-        alt: 'Imagen proyecto 1',
+        titulo: 'Buscador',
+        imagenURL: './public/images/proyecto-3.PNG',
+        alt: 'Imagen proyecto 3',
         botonTexto: 'Ver proyecto',
-        botonURL: './index.html'
+        botonURL: '/PROYECTO-Buscador/index.html'
     },
     {
         id: 4,
-        titulo: 'Proyecto 4',
-        imagenURL: './public/images/veterinaria1.jpg',
-        alt: 'Imagen proyecto 1',
+        titulo: 'Local Storage',
+        imagenURL: './public/images/proyecto-4.PNG',
+        alt: 'Imagen proyecto 4',
         botonTexto: 'Ver proyecto',
-        botonURL: './index.html'
+        botonURL: '/PROYECTO-LocalStorage/index.html'
+    },
+    {
+        id: 5,
+        titulo: 'Cotizar',
+        imagenURL: './public/images/proyecto-5.PNG',
+        alt: 'Imagen proyecto 5',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-Prototypes-Seguro/index.html'
+    },
+    {
+        id: 6,
+        titulo: 'Gasto Semanal',
+        imagenURL: './public/images/proyecto-6.PNG',
+        alt: 'Imagen proyecto 6',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-GastoSemanal/index.html'
+    },
+    {
+        id: 7,
+        titulo: 'Admin Citas',
+        imagenURL: './public/images/proyecto-7.PNG',
+        alt: 'Imagen proyecto 7',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-AdministrarCitas/index.html'
+    },
+    {
+        id: 8,
+        titulo: 'Admin Clientes',
+        imagenURL: './public/images/proyecto-8.PNG',
+        alt: 'Imagen proyecto 8',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-CRMIndexedDB/index.html'
+    },
+    {
+        id: 9,
+        titulo: 'Buscador Recetas',
+        imagenURL: './public/images/proyecto-9.PNG',
+        alt: 'Imagen proyecto 9',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-BuscadorRecetas/index.html'
+    },
+    {
+        id: 10,
+        titulo: 'Buscador Photos',
+        imagenURL: './public/images/proyecto-10.PNG',
+        alt: 'Imagen proyecto 10',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-PixabayImagenes/index.html'
+    },
+    {
+        id: 11,
+        titulo: 'Criptomonedas',
+        imagenURL: './public/images/proyecto-11.PNG',
+        alt: 'Imagen proyecto 11',
+        botonTexto: 'Ver proyecto',
+        botonURL: '/PROYECTO-Criptomonedas/index.html'
     }
 ];
